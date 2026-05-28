b93ad8664cdd5185ac6b29bd0f45512c697458c3
Pedro <pdiaza@miumg.edu.gt>
2026-05-27 19:33:59 -0600

    Primer commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Pruebas de funcionamiento";
+    return 0;
+}
---
+++ pruebas.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int opcion;
+
+    cout << "Ingrese un numero: ";
+    cin >> opcion;
+
+    cout << "Usted ingreso: " << opcion << endl;
+
+    return 0;
+}
