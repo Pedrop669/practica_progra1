fe0a8737c825dbe337f107fb32f4fbca40980c9b
Pedro <pdiaza@miumg.edu.gt>
2026-05-27 19:50:07 -0600

    Segundo commit en desarrollo

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
     cout << "Pruebas de funcionamiento"<<endl;
-    cout << "Rama de prueba"; 
+    cout << "Rama de prueba"<<endl; 
+    cout << "Rama de prueba 2"; 
     return 0;
 }
